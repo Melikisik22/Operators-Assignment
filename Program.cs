caed14ed2f11efb430f97b504d4b59c58cf70318
Melikisik22 <melikisik22@gmail.com>
2026-03-16 22:36:04 +0500

    operator Assignment

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EmployeeComparisonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee emp1 = new Employee()
+            {
+                Id = 101,
+                FirstName = "Ali",
+                LastName = "Khan"
+            };
+
+
+            Employee emp2 = new Employee()
+            {
+                Id = 101,
+                FirstName = "Ahmed",
+                LastName = "Raza"
+            };
+
+            bool areEqual = emp1 == emp2;
+
+            Console.WriteLine("Are the two employees equal? " + areEqual);
+
+            bool areNotEqual = emp1 != emp2;
+
+            Console.WriteLine("Are the two employees NOT equal? " + areNotEqual);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
